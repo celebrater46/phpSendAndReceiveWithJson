fc0801f97cdcbfadd780a5fb33cd2ef9b19d349e
hideru <jota.16.stigma.world@gmail.com>
2021-08-18 02:41:27 +0900

    POST Succeeded

--- receive.php
+++ receive.php
@@ -1,6 +1,17 @@
 <?php
 
+ini_set('display_errors', "On");
+
+// https://qiita.com/hidepy/items/42220523cb2b3eb2c451 【PHP】JSONでPOSTされた値の取り出し方。file_get_contents("php://input") するようだ。
+
 // JSON用
 $json = file_get_contents("php://input"); // POSTされたJSON文字列を取り出し
 $contents = json_decode($json, true); // JSON文字列をobjectに変換（第2引数をtrueにしないとハマるので注意）
 var_dump($contents);
+//var_dump($json);
+
+//echo "Hello World";
+
+//foreach($contents as $item) {
+//    var_dump($item);
+//}
--- send.php
+++ send.php
@@ -2,7 +2,8 @@
 
 ini_set('display_errors', "On");
 
-$url = 'http://localhost/receive.php';
+//$url = 'http://localhost/receive.php';
+$url = 'http://localhost/myapps/phpSendAndReceiveWithJson/receive.php';
 //$url = 'http://localhost:44395/api/members';
 
 $data = array(
@@ -11,24 +12,18 @@ $data = array(
     'HireDate' => '2018-06-28T00:00:00',
 );
 
-$data = json_encode($data); // JSONに変換
+$json = json_encode($data); // JSONに変換
 
 $context = array(
     'http' => array(
-//    'ssl' => array(
         'method'  => 'POST',
-//        'header'  => implode("\r\n", array('Content-Type: application/x-www-form-urlencoded',)),
         'header'  => implode("\r\n", array('Content-Type: application/json',)),
-//        'header'  => 'Content-Type: application/json',
-//        'content' => http_build_query($data),
-        'content' => $data,
-//        'verify_peer' => false,
-//        'verify_peer_name' => false,
+        'content' => $json,
     )
 );
 
 $html = file_get_contents($url, false, stream_context_create($context));
+//file_get_contents($url, false, stream_context_create($context));
 
-var_dump($http_response_header);
-//var_dump($context);
-//echo $html;
+//var_dump($http_response_header);
+echo $html;
